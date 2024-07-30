11871e212d99050dfc233d57e7a1a34dbfa861ce
Atul16raj <atul72210@gmail.com>
2024-07-30 15:15:48 +0530

    HII

---
+++ array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <title>Array Practice</title>
+</head>
+<body>
+    <?php
+       $fruits=array("apple","banana","orange");
+       echo "<pre>";print_r($fruits);
+
+       $students=array(
+        'name'=>'Atul',
+        'age'=>21,
+        'course'=>'MCA'
+       );
+       echo "<pre>";print_r($students);
+    ?>
+    
+</body>
+</html>
